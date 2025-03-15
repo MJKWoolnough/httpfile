87233ce53ab612e815150f89ac39f6c0643a3247
Michael Woolnough <michael.woolnough@gmail.com>
2025-03-15 21:32:41 +0000

    added File that acts as a http.Handler and can take data that it will compress to optionally send gzip'd

---
+++ file.go
@@ -0,0 +1,94 @@
+package httpfile
+
+import (
+	"bytes"
+	"compress/gzip"
+	"io"
+	"io/fs"
+	"net/http"
+	"sync"
+	"time"
+
+	"vimagination.zapto.org/httpencoding"
+)
+
+type File struct {
+	name string
+
+	mu               sync.RWMutex
+	modtime          time.Time
+	data, compressed []byte
+}
+
+func New(name string) *File {
+	return &File{name: name, modtime: time.Now()}
+}
+
+var isGzip = httpencoding.HandlerFunc(func(enc httpencoding.Encoding) bool { return enc == "gzip" })
+
+func (f *File) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var buf bytes.Reader
+
+	wantsGzip := httpencoding.HandleEncoding(r, isGzip)
+
+	f.mu.RLock()
+
+	modtime := f.modtime
+
+	if wantsGzip {
+		buf.Reset(f.compressed)
+		w.Header().Add("Content-Encoding", "gzip")
+	} else {
+		buf.Reset(f.data)
+	}
+
+	f.mu.RUnlock()
+
+	http.ServeContent(w, r, f.name, modtime, &buf)
+}
+
+type file struct {
+	file *File
+	data []byte
+}
+
+func (f *File) Create() io.WriteCloser {
+	return &file{
+		file: f,
+	}
+}
+
+func (f *file) Write(p []byte) (int, error) {
+	if f.file == nil {
+		return 0, fs.ErrClosed
+	}
+
+	f.data = append(f.data, p...)
+
+	return len(p), nil
+}
+
+func (f *file) Close() error {
+	if f.file == nil {
+		return fs.ErrClosed
+	}
+
+	var compressed bytes.Buffer
+
+	g := gzip.NewWriter(&compressed)
+
+	g.Write(f.data)
+	g.Close()
+
+	f.file.mu.Lock()
+
+	f.file.data = f.data
+	f.file.compressed = compressed.Bytes()
+	f.file.modtime = time.Now()
+
+	f.file.mu.Unlock()
+
+	*f = file{}
+
+	return nil
+}
